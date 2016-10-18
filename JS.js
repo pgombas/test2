8a48f40d4a50ca5b01c2b1b87ca33164a96f82b1
Philip Gombas <p.gombas97@htl-ottakring.ac.at>
2016-10-18 11:09:24 +0200

    Initial commit

---
+++ JS.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Philip on 27.09.2016.
+ */
+
+//region DOM
+var thing = document.createElement("p");
+thing.id="1";
+thing.innerHTML=" TOTAL NÜTZLICHER TEXT";
+thing.style.backgroundColor="blue";
+thing.style.color="white";
+document.body.appendChild(thing);
+
+var link = document.createElement("a");
+link.innerHTML="<br> TOTAL NÜTZLICHER LINK";
+link.style.backgroundColor="green";
+link.style.color="white";
+link.href="https://www.youtube.com";
+document.getElementById("1").appendChild(link);
+window.console.log("hi");
+
+//endregion
